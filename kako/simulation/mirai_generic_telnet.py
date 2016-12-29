e5266c4543b41d4cbab1581813496782ace5dc53
Peter Adkins <peter.adkins@kernelpicnic.net>
2016-12-28 20:08:03 -0500

    Mirai specific command handlers.

--- kako/simulation/mirai_generic_telnet.py
+++ kako/simulation/mirai_generic_telnet.py
@@ -7,11 +7,40 @@ from kako.simulation.system import linux
 
 
 class CommandInterpreter(linux.CommandInterpreter):
-    ''' Implements simulation specific command interpretation. '''
+    ''' Implements Mirai specific command interpretation. '''
 
-    def do_derp(self, args=[]):
-        ''' Returns `derp` command output. '''
-        return 'Derp.'
+    def do_wget(self, args=[]):
+        ''' Implement `wget` stub. '''
+        return
+
+    def do_cat(self, args=[]):
+        ''' Implement expected Mirai files. '''
+        if len(args) < 1:
+            return
+
+        # TELNET_DETECT_ARCH - Fake ELF header.
+        if args[0] == '/bin/echo':
+            return ''.join([
+                '\x7F\x45\x4c\x46',                  # ELF magic.
+                '\x01',                              # 32-Bit.
+                '\x01',                              # Little Endian.
+                '\x01',                              # Version 1.
+                '\x03',                              # Linux ABI.
+                '\x00\x00\x00\x00\x00\x00\x00\x00',  # Unused padding.
+                '\x02',                              # Executable.
+                '\x08'                               # MIPS.
+            ])
+
+        # TELNET_PARSE_MOUNTS - Fake read-write mount-point(s).
+        if args[0] == '/proc/mounts':
+            return 'rootfs / rootfs rw 0 0'
+
+    def do_ps(self, args=[]):
+        ''' Implements Mirai expected processes. '''
+        process_list = []
+        process_list.append("PID   Uid    VmSize    Stat    Command\r\n")
+        process_list.append("  1   root      404     S      init")
+        return ''.join(process_list)
 
 
 class RequestHandler(telnet.RequestHandler):
--- kako/simulation/system/linux.py
+++ kako/simulation/system/linux.py
@@ -7,6 +7,13 @@ class CommandInterpreter(object):
     ''' Implements commands for an embedded BusyBox system (Linux). '''
     version = 'BusyBox v1.19.3 (2013-11-01 10:10:26 CST)'
 
+    def do_busybox(self, args=[]):
+        ''' Wrapper the handle() method to handle BusyBox applets. '''
+        if len(args) > 0:
+            return self.handle(' '.join(args))
+        else:
+            return self.version
+
     def do_echo(self, args=[]):
         ''' Returns the input as provided. '''
         return ' '.join(args).strip()
@@ -19,12 +26,21 @@ class CommandInterpreter(object):
         ''' Returns an exit signal to the caller. '''
         raise error.ClientCommandExit()
 
-    def do_busybox(self, args=[]):
-        ''' Wrapper the handle() method to handle BusyBox applets. '''
-        if len(args) > 0:
-            return self.handle(' '.join(args))
-        else:
-            return self.version
+    def do_cat(self, args=[]):
+        ''' Stub `cat`. '''
+        return
+
+    def do_rm(self, args=[]):
+        ''' Stub `rm`. '''
+        return
+
+    def do_cp(self, args=[]):
+        ''' Stub `cp`. '''
+        return
+
+    def do_cd(self, args=[]):
+        ''' Stub `cd`. '''
+        return
 
     def handle(self, command):
         ''' Dispatches the input command to the relevant handler. '''
@@ -32,14 +48,22 @@ class CommandInterpreter(object):
         if len(args) < 1:
             return
 
-        # If it exists, Remove full path prefix from command.
+        # If present, remove full path prefix from command.
         cmd = args[0].split('/')[-1]
         args.pop(0)
 
+        # Attempt to locate a command handler for the read command.
         try:
             ref = getattr(self, "do_{}".format(cmd))
-            return '{}\r\n'.format(ref(args).strip())
         except AttributeError:
             if len(cmd) < 1:
                 return ''
             return "sh: {}: command not found\r\n".format(cmd)
+
+        # Call found handler with provided arguments, and write response back
+        # to socket.
+        result = ref(args)
+        if result:
+            return '{}\r\n'.format(result.strip())
+        else:
+            return '\r\n'
