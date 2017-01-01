96289a0a61bf7c50f662e7c979b46fdf15121885
Peter Adkins <peter.adkins@kernelpicnic.net>
2017-01-01 15:43:21 -0800

    Added unknown_generic_http.

--- kako/simulation/__init__.py
+++ kako/simulation/__init__.py
@@ -1,3 +1,4 @@
 from kako.simulation import server
+from kako.simulation import unknown_generic_http
 from kako.simulation import mirai_generic_telnet
 from kako.simulation import mirai_cpeserver_soap
